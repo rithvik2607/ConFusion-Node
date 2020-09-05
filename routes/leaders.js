02afe9c7bad6dd6c07be2efa8623f50b00d641ff
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-09-06 00:37:21 +0530

    Node Express Assignment-2

--- routes/leaders.js
+++ routes/leaders.js
@@ -1,49 +1,78 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Leaders = require('../models/leaders');
 const leaderRouter = express.Router();
 
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all((req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-})
 .get((req,res,next) => {
-  res.end('Will send all the leaders to you');  
+  Leaders.find({})
+  .then((leaders) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(leaders);
+  }, (err) => next(err))
+  .catch((err) => next(err)); 
 })
 .post((req,res,next) => {
-  res.statusCode = 403;
-  res.end('POST function is not supported by /leaders');
+  Leaders.create(req.body)
+  .then((leader) => {
+    console.log('Leader added', leader);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(leader);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
 .put((req,res,next) => {
-  res.end('Will add the leader: '+ req.body.name +' with details: '+ req.body.description);
+  res.statusCode = 403;
+  res.end('PUT function is not supported by /leaders');
 })
 .delete((req,res,next) => {
-  res.end('Deleting all leaders');
+  Leaders.remove({})
+  .then((resp) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application-json');
+    res.json(resp);
+  }, (err) =>  next(err))
+  .catch((err) => next(err));
 });
 
 leaderRouter.route('/:leaderId')
-.all((req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-})
 .get((req,res,next) => {
-  res.end('Will send the leader: '+ req.params.leaderId +' to you');  
+  Leaders.findById(req.params.leaderId)
+  .then((leader) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(leader);
+  }, (err) => next(err))
+  .catch((err) => next(err));  
 })
 .post((req,res,next) => {
   res.statusCode = 403;
   res.end('POST function is not supported by /leaders/'+ req.params.leaderId);
 })
 .put((req,res,next) => {
-  res.write('Updating the leader: '+ req.params.leaderId +'\n');
-  res.end('Will update the leader name as: '+ req.body.name +' and details as: '+ req.body.description);
+  Leaders.findByIdAndUpdate(req.params.leaderId,{
+    $set: req.body
+  },{ new: true })
+  .then((leader) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(leader);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
 .delete((req,res,next) => {
-  res.end('Deleting the leader: '+ req.params.leaderId);
+  Leaders.findByIdAndRemove(req.params.leaderId)
+  .then((resp) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
 module.exports = leaderRouter;
--- routes/promotions.js
+++ routes/promotions.js
@@ -1,49 +1,77 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Promos = require('../models/promotions');
 const promotionsRouter = express.Router();
 
 promotionsRouter.use(bodyParser.json());
 
 promotionsRouter.route('/')
-.all((req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-})
 .get((req,res,next) => {
-  res.end('Will send all the promotions to you');  
+  Promos.find({})
+  .then((promos) => {
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'application/json');
+   res.json(promos); 
+  }, (err) =>  next(err))
+  .catch((err) => next(err));
 })
 .post((req,res,next) => {
-  res.statusCode = 403;
-  res.end('POST function is not supported by /promotions');
+  Promos.create(req.body)
+  .then((promo) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(promo);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
 .put((req,res,next) => {
-  res.end('Will add the promotion: '+ req.body.name +' with details: '+ req.body.description);
+  res.statusCode = 403;
+  res.end('PUT function is not supported by /promotions');
 })
 .delete((req,res,next) => {
-  res.end('Deleting all promotions');
+  Promos.remove({})
+  .then((resp) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
 promotionsRouter.route('/:promoId')
-.all((req,res,next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-})
 .get((req,res,next) => {
-  res.end('Will send the promotion: '+ req.params.promoId +' to you');  
+  Promos.findById(req.params.promoId)
+  .then((promo) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(promo);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
 .post((req,res,next) => {
   res.statusCode = 403;
   res.end('POST function is not supported by /promotions/'+ req.params.promoId);
 })
 .put((req,res,next) => {
-  res.write('Updating the promotion: '+ req.params.promoId +'\n');
-  res.end('Will update the promotion name as: '+ req.body.name +' and details as: '+ req.body.description);
+  Promos.findByIdAndUpdate(req.params.promoId,{
+    $set: req.body
+  }, { new: true })
+  .then((promo) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(promo);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 })
 .delete((req,res,next) => {
-  res.end('Deleting the promotion: '+ req.params.promoId);
+  Promos.findByIdAndRemove(req.params.promoId)
+  .then((resp) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(resp);
+  }, (err) => next(err))
+  .catch((err) => next(err));
 });
 
 module.exports = promotionsRouter;
