f06abac33ecde79937a1b60878ee3ab2de443fe1
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-09-21 19:47:19 +0530

    Passport Facebook

--- routes/users.js
+++ routes/users.js
@@ -67,6 +67,15 @@ router.get('/logout', (req,res) => {
     err.status = 403;
     next(err);
   }
-})
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req,res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({status: 'You are successfully logged in!', token: token, success: true});
+  }
+});
 
 module.exports = router;
